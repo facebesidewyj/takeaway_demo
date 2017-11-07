8528081cae20ab790fb9ba8a1e9ebbb5710df1cd
facebesidewyj <282719503@qq.com>
2017-11-07 22:50:01 +0800

    完成商品页的布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,14 +3,39 @@
         <div class="menu-wrapper">
             <ul>
                 <li v-for="item in goods" class="menu-item">
-                    <span class="text">
+                    <span class="text border-1px">
                         <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>
                         {{item.name}}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper"></div>
+        <div class="foods-wrapper">
+          <ul>
+            <li class="food-list" v-for="item in goods">
+              <h1 class="title">{{item.name}}</h1>
+              <ul>
+                <li class="food-item border-1px" v-for="food in item.foods">
+                  <div class="icon">
+                    <img :src="food.icon" width="57" height="57" alt="商品图标">
+                  </div>
+                  <div class="content">
+                    <h2 class="food-name">{{food.name}}</h2>
+                    <p class="desc" v-show="food.description">{{food.description}}</p>
+                    <div class="extra">
+                      <span>月售{{food.sellCount}}份</span>
+                      <span>好评率{{food.rating}}%</span>
+                    </div>
+                    <div class="price">
+                      <span class="now">￥{{food.price}}</span>
+                      <span class="old" v-show="food.oldPrice">￥{{food.price}}</span>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
     </div>
 </template>
 
@@ -64,7 +89,6 @@
             vertical-align: top
             width: 12px
             height 12px
-            margin-right: 2px
             background-size: 12px 12px
             background-repeat: no-repeat
             &.decrease
@@ -85,4 +109,51 @@
             border-1px(rgba(7, 17, 27, 0.2))
     .foods-wrapper
       flex: 1
+      .title
+        height: 26px
+        padding-left: 14px
+        border-left: 2px solid #d9dde1
+        line-height: 26px
+        background-color: #f3f5f7
+        color: rgb(147, 153, 159)
+        font-size: 12px
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.2))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          width: 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .food-name
+            margin: 2px 0 8px 0
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            span:nth-child(1)
+              margin-right: 12px
+          .price
+            font-weight: 700
+            height-line: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 </style>
